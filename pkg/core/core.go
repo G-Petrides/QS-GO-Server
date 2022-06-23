06d416fe6c4a53c5ece2a8e33e3b045150729e9d
Geoff <geoff.petrides@gmail.com>
2022-06-23 10:07:09 +0100

    minor formatting tweaks

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -2,25 +2,37 @@ package core
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 )
-
-type Config struct {
-	ApplicationId     string
+//LinnConfig : struct for parsing Linnworks API data
+//from the config.json file in the root folder.
+type LinnConfig struct {
+    ApplicationID     string    `json:"applicationId"`
 	ApplicationSecret string
 	Token             string
-	DbAddress         string
-	DbPort            string
 }
 
+//DbConfig : struct for parsing Database connection settings
+//from the config.json file in the root folder.
+type DbConfig struct {
+	DbAddress string
+	DbPort    string
+}
+
+//Config : struct for unmarshaling config.json
+type Config struct {
+	Linn LinnConfig
+	Db   DbConfig
+}
+
+//File is a global setting with the path to config.json
+//Allows setting of a relative path for unit testing.
 var File = "config.json"
 
+//GetConfig reads config.json, unmarshals and returns the config data.
 func GetConfig() (Config, error) {
 	config := Config{}
 	file, err := ioutil.ReadFile(File)
-
-	fmt.Println(string(file))
 
 	if err != nil {
 		return config, err
--- pkg/linn/linn_test.go
+++ pkg/linn/linn_test.go
@@ -5,14 +5,23 @@ import (
 	"testing"
 )
 
-func TestAuth(t *testing.T) {
+func TestInit(t *testing.T) {
 	core.File = "../../config.json"
 	config, err := core.GetConfig()
 	if err != nil {
 		t.Errorf("Core config fail: %s", err.Error())
 	}
-	err = Auth(config)
-	if err != nil {
-		t.Errorf("Linn Auth error: %s", err.Error())
+
+	var done = make(chan InitResult)
+	go Init(config, done)
+
+	result := <-done
+	if result.Err != nil {
+		t.Errorf("Linn init result error: %s", result.Err.Error())
+	}
+
+	t.Log(result.authData.Server + " - " + result.authData.Token)
+	if result.authData.Server == "" || result.authData.Token == "" {
+		t.Error("Missing Linn.authProperties")
 	}
 }
